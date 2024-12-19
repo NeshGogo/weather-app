517f2062ca08755f94db1bd3f2864948f985c918
NeshGogo <rafaelaguero10@hotmail.com>
2024-12-18 20:15:03 -0400

    Making frontend to point to local backend

--- weatherWeb/src/app/services/weather.service.ts
+++ weatherWeb/src/app/services/weather.service.ts
@@ -8,19 +8,17 @@ import { Weather } from '../models/weather';
   providedIn: 'root',
 })
 export class WeatherService {
-  private readonly language = 'en';
-  private readonly sections = 'all';
   constructor(private http: HttpClient) {}
 
   getWeather(placeId: string, units: string) {
     return this.http.get<Weather>(
-      `${environment.api}/point?place_id=${placeId}&units=${units}&sections=${this.sections}&language=${this.language}&key=${environment.apiKey}`
+      `${environment.api}/weather/${placeId}?units=${units}`
     );
   }
 
   getPlaces(text: string) {
     return this.http.get<Place[]>(
-      `${environment.api}/find_places_prefix?text=${text}&language=${this.language}&key=${environment.apiKey}`
+      `${environment.api}/places?searchTerm=${text}`
     );
   }
 }
--- weatherWeb/src/environments/environment.example.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  api: 'https://www.meteosource.com/api/v1/free/',
-  apiKey: '',
-};
