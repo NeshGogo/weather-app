b66d87f8c8f10deb00012aa4a05bd941e95be9ba
NeshGogo <rafaelaguero10@hotmail.com>
2024-12-23 23:14:32 -0400

    Add AI-powered recommendations to summary section component

--- weatherWeb/src/app/components/summary-section/summary-section.component.ts
+++ weatherWeb/src/app/components/summary-section/summary-section.component.ts
@@ -1,34 +1,43 @@
 import { Component, input } from '@angular/core';
+import { SkeletonComponent } from "../skeleton/skeleton.component";
 
 @Component({
   selector: 'app-summary-section',
-  imports: [],
+  imports: [SkeletonComponent],
   template: `
     <div
-      class="flex justify-between items-center mt-4 shadow dark:shadow-indigo-700 w-full p-3 bg-white border border-gray-200 rounded-lg dark:bg-indigo-950 dark:border-indigo-800"
+      class="mt-4 shadow dark:shadow-indigo-700 w-full p-3 bg-white border border-gray-200 rounded-lg dark:bg-indigo-950 dark:border-indigo-800"
     >
-      <div>
-        <h1
-          class="uppercase mb-1 text-xl font-extrabold leading-none tracking-tight text-gray-700 md:text-2xl dark:text-white"
-        >
-          {{ name() }}
-        </h1>
-        <p class="mb-3 text-gray-500 dark:text-gray-400">
-          Chance of rain: {{ precipitation() }}%
-        </p>
-        <h1
-          class="text-xl font-extrabold leading-none tracking-tight text-gray-700 md:text-2xl dark:text-white"
-        >
-          {{ temperature()?.toFixed() }}°
-        </h1>
+      <div class="flex justify-between items-center">
+        <div>
+          <h1
+            class="uppercase mb-1 text-xl font-extrabold leading-none tracking-tight text-gray-700 md:text-2xl dark:text-white"
+          >
+            {{ name() }}
+          </h1>
+          <p class="mb-3 text-gray-500 dark:text-gray-400">
+            Chance of rain: {{ precipitation() }}%
+          </p>
+          <h1
+            class="text-xl font-extrabold leading-none tracking-tight text-gray-700 md:text-2xl dark:text-white"
+          >
+            {{ temperature()?.toFixed() }}°
+          </h1>
+        </div>
+        <div>
+          <img
+            class="md:w-32"
+            src="assets/icons/big/{{ icon() }}.png"
+            [alt]="summary()"
+          />
+        </div>
       </div>
-      <div>
-        <img
-          class="md:w-32"
-          src="assets/icons/big/{{icon()}}.png"
-          [alt]="summary()"
-        />
-      </div>
+      <p class="text-gray-600 dark:text-gray-300">AI-Powered Recommendations:</p>
+      @if (recommendations()) {
+        <p class="text-gray-600 dark:text-gray-300">{{ recommendations() }}</p>
+      } @else {
+        <app-skeleton></app-skeleton>
+      }
     </div>
   `,
 })
@@ -38,4 +47,5 @@ export class SummarySectionComponent {
   temperature = input<number>();
   icon = input<number>();
   summary = input<string>();
+  recommendations = input<string>();
 }
