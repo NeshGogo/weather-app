d4015ff6d0efd40b7b02136ae6dee2524eb9cc24
NeshGogo <rafaelaguero10@hotmail.com>
2024-12-25 18:31:38 -0400

    Add unit tests for SummarySectionComponent to verify name, precipitation, temperature, icon, summary, and recommendations

--- weatherWeb/src/app/components/summary-section/summary-section.component.spec.ts
+++ weatherWeb/src/app/components/summary-section/summary-section.component.spec.ts
@@ -20,4 +20,52 @@ describe('SummarySectionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a name', async () => {
+    fixture.componentRef.setInput('name', 'Place 1');
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const element = fixture.nativeElement.querySelector('h1');
+    expect(element.innerText).toContain('PLACE 1');
+  });
+
+  it('should have a precipitation', async () => {
+    fixture.componentRef.setInput('precipitation', 50);
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const element = fixture.nativeElement.querySelector('p');
+    expect(element.innerText).toContain('Chance of rain: 50%');
+  });
+
+  it('should have a temperature', async () => {
+    fixture.componentRef.setInput('temperature', 20);
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const elements = fixture.nativeElement.querySelectorAll('h1');
+    expect(elements[1].innerText).toContain('20°');
+  });
+
+  it('should have an icon', async () => {
+    fixture.componentRef.setInput('icon', 1);
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const element = fixture.nativeElement.querySelector('img');
+    expect(element.getAttribute('src')).toContain('assets/icons/big/1.png');
+  });
+
+  it('should have a summary', async () => {
+    fixture.componentRef.setInput('summary', 'Summary');
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const element = fixture.nativeElement.querySelector('img');
+    expect(element.getAttribute('alt')).toContain('Summary');
+  });
+
+  it('should have recommendations', async () => {
+    fixture.componentRef.setInput('recommendations', 'Recommendations');
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const elements = fixture.nativeElement.querySelectorAll('p');
+    expect(elements[1].innerText).toContain('Recommendations');
+  });
 });
