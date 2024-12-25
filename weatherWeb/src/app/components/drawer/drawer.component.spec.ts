e8228fb82184cdc5d2e426cd0d6f11214397ce25
NeshGogo <rafaelaguero10@hotmail.com>
2024-12-25 17:42:10 -0400

    Add unit tests for DrawerComponent to verify show/hide functionality and title setting

--- weatherWeb/src/app/components/drawer/drawer.component.spec.ts
+++ weatherWeb/src/app/components/drawer/drawer.component.spec.ts
@@ -20,4 +20,42 @@ describe('DrawerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show the drawer', () => {
+    expect(component.show()).toBeFalse();
+
+    fixture.componentRef.setInput('show', true);
+    fixture.detectChanges();
+
+    expect(component.show()).toBeTrue();
+    expect(fixture.debugElement.nativeElement.querySelector('.transform-none')).toBeTruthy();
+  });
+
+  it('should hide the drawer', () => {
+    expect(component.show()).toBeFalse();
+
+    fixture.componentRef.setInput('show', false);
+    fixture.detectChanges();
+
+    expect(component.show()).toBeFalse();
+    expect(fixture.debugElement.nativeElement.querySelector('.translate-x-full')).toBeTruthy();
+  });
+
+  it('should emit the close event', () => {
+    let closed = false;
+    component.close.subscribe(() => closed = true);
+
+    component.close.emit();
+    expect(closed).toBeTrue();
+  });
+
+  it('should set the title', () => {
+    expect(component.title()).toBe('');
+
+    fixture.componentRef.setInput('title', 'Drawer Title');
+    fixture.detectChanges();
+
+    expect(component.title()).toBe('Drawer Title');
+    expect(fixture.debugElement.nativeElement.querySelector('h5').innerText).toContain('Drawer Title');
+  });
 });
