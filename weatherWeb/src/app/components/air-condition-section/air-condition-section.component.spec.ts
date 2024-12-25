f40bbfde7daf3676216a14013ce69b051ebc3fc2
NeshGogo <rafaelaguero10@hotmail.com>
2024-12-25 17:26:53 -0400

    Add unit test for AirConditionSectionComponent to verify weather display

--- weatherWeb/src/app/components/air-condition-section/air-condition-section.component.spec.ts
+++ weatherWeb/src/app/components/air-condition-section/air-condition-section.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AirConditionSectionComponent } from './air-condition-section.component';
+import { By } from '@angular/platform-browser';
 
 describe('AirConditionSectionComponent', () => {
   let component: AirConditionSectionComponent;
@@ -8,9 +8,8 @@ describe('AirConditionSectionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AirConditionSectionComponent]
-    })
-    .compileComponents();
+      imports: [AirConditionSectionComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AirConditionSectionComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,26 @@ describe('AirConditionSectionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display ari conditions values', async () => {
+    const weather = {
+      temperature: 25,
+      icon: 2,
+      wind: 10,
+      precipitation: 30,
+      summary: 'sunny',
+      hourly: [],
+      daily: [],
+    };
+
+    fixture.componentRef.setInput('weather', weather);
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const elements = fixture.debugElement.queryAll(By.css('.font-bold'));
+
+    expect(elements[0].nativeNode.innerText).toContain('25°');
+    expect(elements[1].nativeNode.innerText).toContain('10');
+    expect(elements[2].nativeNode.innerText).toContain('30');
   });
 });
