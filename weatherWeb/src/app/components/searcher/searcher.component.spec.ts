2f5c70cb120c7b334e13dd7a989d4cda005df61d
NeshGogo <rafaelaguero10@hotmail.com>
2024-12-25 18:19:13 -0400

    Add unit tests for SearcherComponent to verify places handling and search functionality

--- weatherWeb/src/app/components/searcher/searcher.component.spec.ts
+++ weatherWeb/src/app/components/searcher/searcher.component.spec.ts
@@ -9,9 +9,8 @@ describe('SearcherComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SearcherComponent]
-    })
-    .compileComponents();
+      imports: [SearcherComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SearcherComponent);
     component = fixture.componentInstance;
@@ -20,5 +19,45 @@ describe('SearcherComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have places', async () => {
+    const places: Place[] = [
+      { name: 'Place 1', id: '1' },
+      { name: 'Place 2', id: '2' },
+      { name: 'Place 3', id: '2' },
+    ];
+
+    component.showOptions.set(true);
+    fixture.componentRef.setInput('items', places);
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const elements = fixture.nativeElement.querySelectorAll('.cursor-pointer');
+    expect(elements[0].innerText).toContain(places[0].name);
+    expect(elements[1].innerText).toContain(places[1].name);
+    expect(elements[2].innerText).toContain(places[2].name);
+  });
+
+  it('should select a place', async () => {
+    const place: Place = { name: 'Place 1', id: '1' };
+
+    component.selected.subscribe((selected) => {
+      expect(selected).toEqual(place);
+    });
+
+    component.selectedItem(place);
+  });
+
+  it('should search a place',  (done) => {
+    const searchValue = 'Place 1';
+
+    component.onKeyUp.subscribe((text) => {
+      expect(text).toEqual(searchValue);
+      done();
+    });
+
+    const event = { target: { value: searchValue }, preventDefault: () => {} };
+    component.searchItem(event);
   });
 });
