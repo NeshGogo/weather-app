2deea5b5b3401e0769581ef0f1b534bff1bb6857
NeshGogo <rafaelaguero10@hotmail.com>
2024-12-26 21:19:23 -0400

    Add unit tests for WeatherProgressViewComponent to verify title, progressions rendering, and correct values

--- weatherWeb/src/app/components/weather-progress-view/weather-progress-view.component.spec.ts
+++ weatherWeb/src/app/components/weather-progress-view/weather-progress-view.component.spec.ts
@@ -20,4 +20,42 @@ describe('WeatherProgressViewComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('title should be empty', () => {
+    expect(component.title()).toBe('');
+  });
+
+  it('progressions should be empty', () => {
+    expect(component.progressions()).toEqual([]);
+  });
+
+  it('should render title', async() => {
+    fixture.componentRef.setInput('title', 'Weather Progress View');
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(fixture.nativeElement.querySelector('h1').textContent).toContain('Weather Progress View');
+  });
+
+  it('should render progressions', async() => {
+    fixture.componentRef.setInput('progressions', [
+      { title: 'Title 1', icon: 'icon1', iconDescription: 'icon1 description', value: 'value1' },
+      { title: 'Title 2', icon: 'icon2', iconDescription: 'icon2 description', value: 'value2' },
+    ]);
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(fixture.nativeElement.querySelectorAll('p').length).toBe(4);
+  });
+
+  it('should render progressions with correct values', async() => {
+    fixture.componentRef.setInput('progressions', [
+      { title: 'Title 1', icon: 'icon1', iconDescription: 'icon1 description', value: 'value1' },
+      { title: 'Title 2', icon: 'icon2', iconDescription: 'icon2 description', value: 'value2' },
+    ]);
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(fixture.nativeElement.querySelectorAll('p')[0].textContent).toContain('Title 1');
+    expect(fixture.nativeElement.querySelectorAll('p')[2].textContent).toContain('Title 2');
+    expect(fixture.nativeElement.querySelectorAll('p')[1].textContent).toContain('value1');
+    expect(fixture.nativeElement.querySelectorAll('p')[3].textContent).toContain('value2');
+  });
 });
