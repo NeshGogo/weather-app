4ffdc8e06bd22c2cd5093fee33422ed1a5fa1e21
NeshGogo <rafaelaguero10@hotmail.com>
2024-12-26 21:57:31 -0400

    Add unit tests for LayoutComponent to verify favorite place toggling, weather listening, stopping weather listening, and displaying favorite list

--- weatherWeb/src/app/layout/layout.component.spec.ts
+++ weatherWeb/src/app/layout/layout.component.spec.ts
@@ -20,4 +20,42 @@ describe('LayoutComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should toggle favorite place', () => {
+    component.toggleFavoritePlace();
+    expect(component.favorite).toBeTruthy();
+  });
+
+  it('should listen to weather', (done) => {
+    component.listenToWeather.subscribe(async () => {
+      fixture.componentRef.setInput('isPlaying', true);
+      fixture.detectChanges();
+      await fixture.whenStable();
+      done();
+    });
+    component.listenToWeather.emit();
+    expect(component.isPlaying()).toBeTruthy();
+  });
+
+  it('should stop listening to weather', (done) => {
+    component.stopToListen.subscribe(async () => {
+      fixture.componentRef.setInput('isPlaying', false);
+      fixture.detectChanges();
+      await fixture.whenStable();
+      done();
+    });
+    component.stopToListen.emit();
+    expect(component.isPlaying()).toBeFalsy();
+  });
+
+  it('should show favorite list', (done) => {
+    component.showFavoriteList.subscribe(async () => {
+      fixture.componentRef.setInput('thereisAPlace', true);
+      fixture.detectChanges();
+      await fixture.whenStable();
+      done();
+    });
+    component.showFavoriteList.emit(true);
+    expect(component.thereisAPlace()).toBeTruthy();
+  });
 });
